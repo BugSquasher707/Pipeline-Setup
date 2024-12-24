0bb0c8bab90386297e2cdde082260e5c6b55f7e8
BugSquasher707 <arslanarjumand012@gmail.com>
2024-12-24 13:11:49 +0500

    refactor CI/CD workflows to use environment variables for app name and secrets in production and staging

--- ECS-and-ECR-Pipeline/staging/terraform/staging/variables.tf
+++ ECS-and-ECR-Pipeline/staging/terraform/staging/variables.tf
@@ -6,13 +6,11 @@ variable "region" {
 variable "app_name" {
   description = "Name of the application"
   type    = string
-  default = "app_name"
 }
 
 variable "environment" {
   description = "Environment in which the application is deployed"
   type    = string
-  default = "staging"
 }
 
 variable "container_port" {
